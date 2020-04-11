5ce5ca36b6b502388d996a97a2907b21e795cfca
doribasson <doribasson87@gmail.com>
2020-04-11 22:09:46 +0300

    first1

---
+++ server.js
@@ -0,0 +1,17 @@
+//for starting will type in terminal:  node server  or modemon: npm run server
+
+//in package.json
+// "scripts": {
+//     "start": "node server",
+//     "server": "nodemon server"
+//   },
+
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => res.send("API Running"));
+
+const PORT = process.env.PORT || 5000; //locally 5000 and heroku PORT http://localhost:5000/ or in get postman
+
+app.listen(PORT, () => console.log(`server started on porn ${PORT}`));
