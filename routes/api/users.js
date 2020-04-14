1b71fc1be07b28900d10f7ef1f38b8c5f6f57262
doribasson <doribasson87@gmail.com>
2020-04-14 19:23:51 +0300

    commit6-register-endpoint-with-validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -10,13 +10,32 @@
 
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
 
 //@route    POST api/users
 //@desc     Register user
 //@access   Public (no need token)
-router.post("/", (req, res) => {
-  console.log(req.body);
-  res.send("user route");
-});
+router.post(
+  "/",
+  [
+    check("name", "Name is require")
+      .not()
+      .isEmpty(),
+    check("email", "please include a valid email").isEmail(),
+    check(
+      "password",
+      "please enter a password with 6 or more characters"
+    ).isLength({ min: 6 })
+  ],
+  (req, res) => {
+    // console.log(req.body);
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      //if there are error
+      return res.status(400).json({ errors: errors.array() }); //400= bad request , 200= good ..we get in json
+    }
+    res.send("user route");
+  }
+);
 
 module.exports = router;
