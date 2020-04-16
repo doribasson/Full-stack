e34840a4b213bf5c643c3f5b7a5a609fcee79707
doribasson <doribasson87@gmail.com>
2020-04-16 23:45:03 +0300

    commit12-Profile-endpoint-Get

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -116,9 +116,46 @@ router.post(
   }
 );
 
+//@route    Get api/profile
+//@desc     Get all  profiles
+//@access   Public
+router.get("/", async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate("user", ["name", "avatar"]);
+    res.json(profiles);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+//@route    Get api/profile/user/:user_id
+//@desc     Get profile by user ID
+//@access   Public
+router.get("/user/:user_id", async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id
+    }).populate("user", ["name", "avatar"]);
+
+    if (!profile) return res.status(400).json({ msg: "Profile not found" });
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    //if we type id that not match
+    if (err.kind == "ObjectId") {
+      return res.status(400).json({ msg: "Profile not found" });
+    }
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
 
 //http://localhost:5000/api/profile/me
+
+//http://localhost:5000/api/profile/user/5e976f1da365261708240d9e   5e976f1da365261708240d9e = id
 
 //http://localhost:5000/api/profile
 //in postman raw
