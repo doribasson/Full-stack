6959962c90a68c501b2e14e79bd4006ec964f738
doribasson <doribasson87@gmail.com>
2020-04-19 13:20:37 +0300

    commit18-Add Post Route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,48 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
+const auth = require("../../middleware/auth");
 
-//@route    GET api/posts
-//@desc     Test route
-//@access   Public (no need token)
-router.get("/", (req, res) => res.send("posts route"));
+const Post = require("../../models/Post");
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
+
+//@route    Post api/posts
+//@desc     Create a post
+//@access   Private because you have to be logged in to create a post
+router.post(
+  "/",
+  [
+    auth,
+    [
+      check("text", "Text is required")
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select("-password");
+
+      const newPost = new Post({
+        text: req.body.text, //come from the body
+        name: user.name, //come from user
+        avatar: user.avatar, //come from user
+        user: req.user.id // //come from user we need the ObjectID
+      });
+
+      const post = await newPost.save();
+      res.json(post);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 module.exports = router;
