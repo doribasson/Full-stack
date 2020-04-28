3fbb2b81e47ad7bc02f1657efde6ef3e5bbd4dda
doribasson <doribasson87@gmail.com>
2020-04-29 00:21:03 +0300

    commit60-final-with-deploy-static-heroku

--- server.1.js
+++ server.1.js
@@ -8,7 +8,6 @@
 // **********************************************
 const express = require("express");
 const connectDB = require("./config/db");
-const path = require("path");
 
 const app = express();
 
@@ -18,23 +17,13 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
+app.get("/", (req, res) => res.send("API Running"));
+
 // Define Routes
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
-
-//Serve static assets in production
-if (process.env.NODE_ENV === "production") {
-  //Set static folder
-  app.use(express.static("client/build")); //build folder to be a static
-
-  //anything of the Routes above.. exmppale app.use("/api/users", require("./routes/api/users"));
-  app.get("*", (req, res) => {
-    //to load that html file.. __dirname, we going from current dircetry to client folder and to the build folder and we want to load the index.html
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
 
 const PORT = process.env.PORT || 5000; //locally 5000 and heroku PORT http://localhost:5000/ or in get postman
 
